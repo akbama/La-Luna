4c4ff02d487c94a4e4c71e95e497da5231f8c014
akbama <80558884+akbama@users.noreply.github.com>
2022-12-26 09:05:50 +0800

    update

--- Services/services.js
+++ Services/services.js
@@ -1,4 +1,52 @@
-function manual () {
-    var radio = document.getElementById("manualinput");
-    radio.checked = true;
-}
+function extraguests1 () {
+    var check = document.getElementById("ag1");
+    var text = document.getElementById("nightsnum1");
+    if (check.checked==true) {
+      text.disabled = false;
+      text.style.opacity = 1;
+    }
+    else {
+      text.disabled = true;
+      text.style.opacity = 0.5;
+    }
+  }
+  function extraguests2 () {
+    var check = document.getElementById("ag2");
+    var text = document.getElementById("nightsnum2");
+    if (check.checked==true) {
+      text.disabled = false;
+      text.style.opacity = 1;
+    }
+    else {
+      text.disabled = true;
+      text.style.opacity = 0.5;
+    }
+  }
+  function selectmanual () {
+    var radio = document.getElementById("manual");
+    var text = document.getElementById("manualinput");
+    if (radio.checked==true) {
+      text.disabled = false;
+      text.style.opacity = 1;
+    }
+    else {
+      text.disabled = true;
+      text.style.opacity = 0.5;
+    }
+  }
+  function opencontainer() {
+    var open = document.getElementById("salescontainer");
+    if (window.getComputedStyle(open).display === "none") {
+        open.style.display = "block";
+    }
+    else {
+        open.style.display = "none";
+    }
+  }
+
+$(document).ready(function(){
+    $('ul li a').click(function(){
+    $('li a').removeClass("active");
+    $(this).addClass("active");
+});
+});
