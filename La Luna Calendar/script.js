008de518031a70d9f6a9e256bb4eee8e32373b38
Althea Cepeda <ahcepeda@up.edu.ph>
2023-01-07 07:18:53 +0800

    adding pop up

--- La Luna Calendar/script.js
+++ La Luna Calendar/script.js
@@ -81,3 +81,41 @@ document.querySelector(".next").addEventListener("click", () => {
 });
 
 renderCalendar();
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("days");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+// function to get today's date
+$(document).ready(function(){
+  $('ul li a').click(function(){
+    $('li a').removeClass("active");
+    $(this).addClass("active");
+});
+});     
+
+var dt = new Date();
+document.getElementById("date").innerHTML= dt.toDateString();
