ee35ffee3986239adbeb1514005a63b16dbf858e
jlnnemrzo1 <83994874+jlnnemrzo1@users.noreply.github.com>
2023-01-09 21:53:56 +0800

    push

--- Services/services.php
+++ Services/services.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <link rel="stylesheet" href="../Sidebar Nav/sidebar_style.css">
-  <link rel="stylesheet" href="services.css">
+  <link rel="stylesheet" href="services.css?v=<?php echo time(); ?>">
   <link rel="stylesheet" href="../common design.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
